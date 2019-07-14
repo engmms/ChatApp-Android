25cd090aaadb68ba6137b6b2b27cf587f0b6d966
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-07-14 11:28:22 +0200

    Find users in the database and display them in a RecyclerView

--- app/src/main/java/com/peteralexbizjak/chatappandroid/activities/FindUsers.kt
+++ app/src/main/java/com/peteralexbizjak/chatappandroid/activities/FindUsers.kt
@@ -2,16 +2,23 @@ package com.peteralexbizjak.chatappandroid.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.peteralexbizjak.chatappandroid.MainActivity
 import com.peteralexbizjak.chatappandroid.R
+import com.peteralexbizjak.chatappandroid.adapters.UserRecyclerAdapter
+import com.peteralexbizjak.chatappandroid.interfaces.OnItemClickListener
+import com.peteralexbizjak.chatappandroid.models.UserModel
 
 class FindUsers : AppCompatActivity() {
 
@@ -29,11 +36,21 @@ class FindUsers : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_users)
 
+        //Get instance of FirebaseAuth object
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //Get instance of FirebaseDatabase and DatabaseReference
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
         //Initialize the views
         toolbar = findViewById(R.id.findUserToolbar)
         searchIcon = findViewById(R.id.findUserSearchIcon)
         searchField = findViewById(R.id.findUserSearchField)
         recyclerView = findViewById(R.id.findUserRecyclerView)
+
+        //Set the layout manager for the RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //Setup Toolbar
         setSupportActionBar(toolbar)
@@ -41,11 +58,86 @@ class FindUsers : AppCompatActivity() {
         //Set navigation icon click listener
         toolbar.setNavigationOnClickListener { startActivity(Intent(this@FindUsers, MainActivity::class.java)) }
 
-        //Get instance of FirebaseAuth object
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        //Get instance of FirebaseDatabase and DatabaseReference
-        firebaseDatabase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDatabase.reference.child("users")
+        //Start listening for changes to EditText
+        setupEditTextChangeListener()
     }
+
+    /**
+     * Listener that follows changes when user edits EditText. Initiate query to the database only when user stops
+     * typing and contents are not blank or null.
+     */
+    private fun setupEditTextChangeListener() {
+        searchField.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrBlank()) displayFoundUsers(searchForUsers(s.toString()))
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    /**
+     * Display found users in a RecyclerView with the help of a custom adapter
+     */
+    private fun displayFoundUsers(listOfUsers: MutableList<UserModel>) {
+        val recyclerViewAdapter = UserRecyclerAdapter(this, listOfUsers)
+        recyclerView.adapter = recyclerViewAdapter
+        recyclerViewAdapter.notifyDataSetChanged()
+        recyclerView.addOnItemClickListener(object: OnItemClickListener {
+            override fun onItemClicked(view: View, position: Int) {
+                Toast.makeText(this@FindUsers, "Is ma boi ${listOfUsers[position].displayName}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    /**
+     * Search for users in the database that satisfy the search query, which can either represent display
+     * name or email
+     */
+    private fun searchForUsers(searchQuery: String): MutableList<UserModel> {
+
+        //Declare and instantiate a MutableList of UserModel objects
+        val userModelList: MutableList<UserModel> = ArrayList()
+
+        //Transform searchQuery into lowercase
+        searchQuery.toLowerCase()
+
+        databaseReference.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@FindUsers, "Database error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    val user: UserModel = snapshot.getValue(UserModel::class.java)!!
+                    if (user.id != firebaseAuth.currentUser!!.uid && (user.displayName.toLowerCase().contains(searchQuery) || user.email.toLowerCase().contains(searchQuery)))
+                        userModelList.add(user)
+                }
+            }
+        })
+
+        return userModelList
+    }
+
+    /**
+     * Setup on click listener for RecyclerView
+     */
+    private fun RecyclerView.addOnItemClickListener(onClickListener: OnItemClickListener) {
+        this.addOnChildAttachStateChangeListener(object: RecyclerView.OnChildAttachStateChangeListener {
+
+            override fun onChildViewDetachedFromWindow(view: View) {
+                view.setOnClickListener(null)
+            }
+
+            override fun onChildViewAttachedToWindow(view: View) {
+                view.setOnClickListener {
+                    val holder = getChildViewHolder(view)
+                    onClickListener.onItemClicked(view, holder.adapterPosition)
+                }
+            }
+        })
+    }
+
+
 }
