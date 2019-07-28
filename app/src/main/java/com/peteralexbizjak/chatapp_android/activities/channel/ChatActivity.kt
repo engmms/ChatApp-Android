23b9f41206f6fe4b6f6a3f1875b181194e286c6b
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-07-28 14:19:21 +0200

    Begin working on the chat activity, prepare the activity prototype

--- app/src/main/java/com/peteralexbizjak/chatapp_android/activities/channel/ChatActivity.kt
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/activities/channel/ChatActivity.kt
@@ -1,10 +1,21 @@
 package com.peteralexbizjak.chatapp_android.activities.channel
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.peteralexbizjak.chatapp_android.R
 
 class ChatActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+
+    companion object {
+        private val nonEmptyColor: Int = Color.parseColor("#f44336")
+        private val emptyColor: Int = Color.parseColor("#666666")
     }
 }
