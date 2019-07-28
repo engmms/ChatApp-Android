f8f78c88d066661dbb3282b27e89666731796fec
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-07-28 11:30:16 +0200

    Add welcome and sign up activities source file prototypes

--- app/src/main/java/com/peteralexbizjak/chatapp_android/MainActivity.kt
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.peteralexbizjak.chatapp_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.peteralexbizjak.chatapp_android.activities.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: temporary transition, fix later
+        startActivity(Intent(this@MainActivity, WelcomeActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.peteralexbizjak.chatapp_android.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.peteralexbizjak.chatapp_android.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
