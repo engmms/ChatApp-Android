2a0f6c8c16d15c1672a9434e1fc8ae2a11c262db
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-08-03 00:14:58 +0200

    Add account fetching functionality to the app

--- app/src/main/java/com/peteralexbizjak/chatapp_android/activities/navigation/AccountActivity.kt
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/activities/navigation/AccountActivity.kt
@@ -2,16 +2,20 @@ package com.peteralexbizjak.chatapp_android.activities.navigation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.peteralexbizjak.chatapp_android.MainActivity
 import com.peteralexbizjak.chatapp_android.R
+import com.peteralexbizjak.chatapp_android.models.general.UserModel
+import com.peteralexbizjak.chatapp_android.utils.CircleTransform
+import com.squareup.picasso.Picasso
 
 class AccountActivity : AppCompatActivity() {
 
@@ -33,6 +37,7 @@ class AccountActivity : AppCompatActivity() {
 
         prepareFirebase()
         initializeViews()
+        populateAccountData()
     }
 
     private fun prepareFirebase() {
@@ -45,6 +50,7 @@ class AccountActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.accountToolbar)
         setSupportActionBar(toolbar)
         toolbar.setNavigationOnClickListener { startActivity(Intent(this@AccountActivity, MainActivity::class.java)) }
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         profilePic = findViewById(R.id.accountProfileImage)
         profileDisplayName = findViewById(R.id.accountProfileDisplayName)
@@ -52,5 +58,34 @@ class AccountActivity : AppCompatActivity() {
 
         editAccount = findViewById(R.id.accountEditAccountButton)
         editAccount.setOnClickListener { startActivity(Intent(this@AccountActivity, EditAccountActivity::class.java)) }
+    }
+
+    private fun populateAccountData() {
+
+        databaseReference.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot: DataSnapshot in dataSnapshot.children) {
+                    if (snapshot.getValue(UserModel::class.java)?.uid == firebaseAuth.currentUser!!.uid) {
+                        val userModel: UserModel? = snapshot.getValue(UserModel::class.java)
+
+                        if (userModel != null) {
+                            if (userModel.photoUrl != null) Picasso.get().load(userModel.photoUrl).transform(CircleTransform()).into(profilePic)
+                            profileDisplayName.text = userModel.displayName
+
+                            when {
+                                userModel.phoneNumber != null -> profileEmailPhoneNumber.text = "${userModel.email}\n${userModel.phoneNumber}"
+                                else -> profileEmailPhoneNumber.text = userModel.email
+                            }
+                        }
+
+                        break
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@AccountActivity, "Database error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
