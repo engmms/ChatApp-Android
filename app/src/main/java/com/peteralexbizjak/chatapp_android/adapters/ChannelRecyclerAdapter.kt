6c784c85ce97cccd28889aec81b1fc2b367a6eeb
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-08-10 11:31:39 +0200

    UI updates when you have an unread message you haven't yet replied back to

--- app/src/main/java/com/peteralexbizjak/chatapp_android/adapters/ChannelRecyclerAdapter.kt
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/adapters/ChannelRecyclerAdapter.kt
@@ -1,11 +1,14 @@
 package com.peteralexbizjak.chatapp_android.adapters
 
 import android.content.Context
+import android.graphics.Typeface
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.peteralexbizjak.chatapp_android.R
 import com.peteralexbizjak.chatapp_android.models.firestore.ChatModel
@@ -28,6 +31,12 @@ class ChannelRecyclerAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val chatModel: ChatModel = chatModelList[position]
+
+        chatModel.latestMessage.values.forEach {
+            if (it.length > 50) holder.recipientLastMessage.text = it.substring(0, 50) + "..."
+            else holder.recipientLastMessage.text = it
+        }
+
         chatModel.participants.forEach {
             if (!it.keys.contains(currentUserId)) {
                 it.values.forEach { participantModel ->
@@ -40,14 +49,27 @@ class ChannelRecyclerAdapter(
                 }
             }
         }
-        chatModel.latestMessage.values.forEach {
-            if (it.length > 50) holder.recipientLastMessage.text = it.substring(0, 50) + "..."
-            else holder.recipientLastMessage.text = it
+
+        if (chatModel.unreadMessagesData != null) {
+            chatModel.unreadMessagesData.forEach {
+                if (it.keys.contains(currentUserId))
+                    it.values.forEach { unreadStatus ->
+                        if (unreadStatus) {
+                            holder.recipientUnreadMarker.isVisible = true
+                            holder.recipientLastMessage.typeface = Typeface.DEFAULT_BOLD
+                        }
+                        else {
+                            holder.recipientUnreadMarker.isVisible = false
+                            holder.recipientLastMessage.typeface = Typeface.DEFAULT
+                        }
+                    }
+            }
         }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val recipientProfilePicture: ImageView = itemView.findViewById(R.id.channelItemImageView)
+        val recipientUnreadMarker: ImageView = itemView.findViewById(R.id.channelItemUnreadMarker)
         val recipientDisplayName: TextView = itemView.findViewById(R.id.channelItemReceiver)
         val recipientLastMessage: TextView = itemView.findViewById(R.id.channelItemLastMessage)
     }
