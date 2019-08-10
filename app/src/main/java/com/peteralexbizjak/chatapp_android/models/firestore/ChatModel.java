86d83a5fa0d5217e7086c55087128606a5eb99bd
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2019-08-10 10:46:34 +0200

    Update chat model

--- app/src/main/java/com/peteralexbizjak/chatapp_android/models/firestore/ChatModel.java
+++ app/src/main/java/com/peteralexbizjak/chatapp_android/models/firestore/ChatModel.java
@@ -7,12 +7,14 @@ public class ChatModel {
     public String chatId;
     public List<HashMap<String, ParticipantModel>> participants;
     public HashMap<String, String> latestMessage;
+    public List<HashMap<String, Boolean>> unreadMessagesData;
 
     public ChatModel() {}
-    public ChatModel(String chatId, List<HashMap<String, ParticipantModel>> participants, HashMap<String, String> latestMessage) {
+    public ChatModel(String chatId, List<HashMap<String, ParticipantModel>> participants, HashMap<String, String> latestMessage, List<HashMap<String, Boolean>> unreadMessagesData) {
         this.chatId = chatId;
         this.participants = participants;
         this.latestMessage = latestMessage;
+        this.unreadMessagesData = unreadMessagesData;
     }
 
     public String getChatId() {
@@ -37,5 +39,13 @@ public class ChatModel {
 
     public void setLatestMessage(HashMap<String, String> latestMessage) {
         this.latestMessage = latestMessage;
+    }
+
+    public List<HashMap<String, Boolean>> getUnreadMessagesData() {
+        return unreadMessagesData;
+    }
+
+    public void setUnreadMessagesData(List<HashMap<String, Boolean>> unreadMessagesData) {
+        this.unreadMessagesData = unreadMessagesData;
     }
 }
